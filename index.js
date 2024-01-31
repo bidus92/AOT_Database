505d8c43bf335d046c9c706ac337547dc9333bc3
bidus92 <bidusblogs@gmail.com>
2024-01-31 15:31:17 -0500

    Bug fixes. Add more API utilization and site connectivity referencing other characters, locations, episodes, etc. from other database entries. TODO: Finish that and add animations for full design completion

--- index.js
+++ index.js
@@ -46,10 +46,12 @@ app.post("/characters", async (req, res)=>
 });
 
 
+
+
 app.post("/character-database", async (req, res)=>
 {
     var id = req.body["id"];
-    const response = await axios.get(`https://api.attackontitanapi.com/characters/${id}`);
+    var response = await axios.get(`https://api.attackontitanapi.com/characters/${id}`);
     const result = response.data;
     res.render("data.ejs",
     {
@@ -67,6 +69,7 @@ app.get("/episodes", async (req, res)=>
     }); 
 });
 
+//episode main page to get list of all available episodes
 app.post("/episodes", async (req, res)=>
 {
     var page = req.body["page"];
@@ -78,11 +81,23 @@ app.post("/episodes", async (req, res)=>
     });
 });
 
+//post to access individual episodes
+app.post("/episode-database", async (req, res)=>
+{
+    var id = req.body["id"];
+    const response = await axios.get(`https://api.attackontitanapi.com/episodes/${id}`);
+    const result = response.data;
+    res.render("data.ejs",
+    {
+       data: result
+    })
+});
+
+//locations page listing all available locations
 app.get("/locations", async (req, res)=>
 {
     const response = await axios.get("https://api.attackontitanapi.com/locations/?page=1");
     const result = response.data.results;
-    console.log(result);
     res.render("locations.ejs",
     {
         locationList: result
@@ -101,17 +116,64 @@ app.post("/locations", async (req, res)=>
     });
 });
 
+//GET to link location database with character database where inheritors are referenced
+app.get("/location-database", async (req, res)=>
+{
+    var id;
+    if(req.body["id"])
+    {
+        const response = await axios.get(`https://api.attackontitanapi.com/characters/${id}`);
+        id = req.body["id"];
+    }
+    else
+    {
+        id = req.body["epID"]
+        const response = await axios.get(`https://api.attackontitanapi.com/episodes/${id}`);
+    }
+    
+    const result = response.data;
+    console.log(id); 
+
+    res.render("data.ejs",
+    {
+       data: result
+    });
+   
+});
+
+//The location database.  
 app.post("/location-database", async (req, res)=>
 {
     var id = req.body["id"];
     const response = await axios.get(`https://api.attackontitanapi.com/locations/${id}`);
     const result = response.data;
+    var notable_inhabitants = [];
+    var names = [];
+    console.log(response.data.notable_inhabitants); 
+    for(let x = 0; x < response.data.notable_inhabitants.length; x++)
+    {
+        //Checks to see if there are numbers in the notable inhabitants, indicating a url to get information from the api from
+        if(/\d/.test(response.data.notable_inhabitants[x]))
+        {
+            notable_inhabitants.push(await axios.get(`${response.data.notable_inhabitants[x]}`))
+            names.push(notable_inhabitants[x].data.name);
+        }
+        //otherwise post string as is
+        else
+        {
+            notable_inhabitants.push(response.data.notable_inhabitants[x]);
+            names.push(notable_inhabitants[x]);
+        }
+        
+    }
     res.render("data.ejs",
     {
-       data: result
+       data: result,
+       notable_inhabitants: names
     })
 });
 
+//organizations main page listing them
 app.get("/organizations", async (req, res)=>
 {
     const response = await axios.get("https://api.attackontitanapi.com/organizations");
@@ -122,6 +184,7 @@ app.get("/organizations", async (req, res)=>
     }); 
 });
 
+//POST to send information from organizations page to get appropriate information for individual organizations
 app.post("/organization-database", async (req, res)=>
 {
     var id = req.body["id"];
@@ -133,6 +196,7 @@ app.post("/organization-database", async (req, res)=>
     })
 });
 
+//GET for list of titans
 app.get("/titans", async (req, res)=>
 {
     const response = await axios.get("https://api.attackontitanapi.com/titans/");
@@ -143,14 +207,42 @@ app.get("/titans", async (req, res)=>
     }); 
 });
 
+
+//GET for titan-database to obtain information for character database where inheritors are referenced
+app.get("/titan-database", async (req, res)=>
+{
+    var id = req.body["id"];
+    const response = await axios.get(`https://api.attackontitanapi.com/characters/${id}`);
+    const result = response.data;
+    console.log(id); 
+    res.render("data.ejs",
+    {
+       data: result
+    })
+   
+});
+
+//the titan database that loads all information needed 
 app.post("/titan-database", async (req, res)=>
 {
     var id = req.body["id"];
     const response = await axios.get(`https://api.attackontitanapi.com/titans/${id}`);
     const result = response.data;
+    const current_inheritor = await axios.get(`${response.data.current_inheritor}`);
+
+    var former_inheritors = [];
+    var names = [];
+    for(let x = 0; x < response.data.former_inheritors.length; x++)
+    {
+        former_inheritors.push(await axios.get(`${response.data.former_inheritors[x]}`))
+        names.push(former_inheritors[x].data.name);
+    }
+
     res.render("data.ejs",
     {
-       data: result
+       data: result,
+       current_inheritor: current_inheritor.data.name,
+       former_inheritors: names
     })
 });
 
