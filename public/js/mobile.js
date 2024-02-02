2b227793d95b90c59202d705c8aa341ecafa2261
bidus92 <bidusblogs@gmail.com>
2024-02-02 12:54:03 -0500

    Fix formatting issue with footer. Add further mobile implementation

---
+++ public/js/mobile.js
@@ -0,0 +1,18 @@
+const headerNavLinks = document.querySelectorAll("header > div.nav-link");
+const menu = document.querySelector("#menu-icon");
+const menuImg = document.querySelector("#menu-icon-img");
+const dropdownMenu = document.querySelector("#dropdown-menu");
+const menuItems = document.querySelectorAll("#dropdown-menu > div");
+var windowWidth = screen.availWidth; 
+if(windowWidth <= 430)
+{
+    menuImg.addEventListener("click", ()=>
+    {
+       menuImg.classList.toggle("menu-pressed");
+       dropdownMenu.classList.toggle("dropdown-activated");
+       for(let x = 0; x < menuItems.length; x++)
+       {
+        menuItems[x].classList.toggle("dropdown-activated");
+       }
+    });
+}
